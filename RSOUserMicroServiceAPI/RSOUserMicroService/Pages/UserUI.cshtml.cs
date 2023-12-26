8b4a9dfdf2f471b7ae8e5a95f4c650c1c3755e67
Aleksander Kovač <aleksander.kovac97@hotmail.com>
2023-12-26 17:59:09 +0100

    /UserUI is now live

---
+++ RSOUserMicroServiceAPI/RSOUserMicroService/Pages/UserUI.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RSOUserMicroService.Pages
+{
+    public class UserUIModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
