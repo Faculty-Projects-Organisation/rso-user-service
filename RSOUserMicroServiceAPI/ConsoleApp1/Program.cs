7ac382ddc3f05ae1ff090b0680c1bd437582b314
shanji97 <aleksander.kovac97@hotmail.ocm>
2024-02-13 22:46:48 +0100

    DONE.

--- RSOUserMicroServiceAPI/ConsoleApp1/Program.cs
+++ RSOUserMicroServiceAPI/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ public static class Program
         var client = new Greeter.GreeterClient(channel);
 
         var reply = client.ReturnUserById(new UserByIdRequest { Id = 4});
-        Console.WriteLine("User: " + reply.UserName + " " + reply.Email + " " + reply.UserAddress + " " + reply.ZipCode + " " + reply.RegisteredOn + " " + reply.Id);
+        Console.WriteLine("UserName: " + reply.UserName + " \nEmail: " + reply.Email + " \nUser Address: " + reply.UserAddress + " \nZip code: " + reply.ZipCode + " \nRegistered on:" + reply.RegisteredOn + " \nId:" + reply.Id);
         Console.ReadKey();
         Console.ReadKey();
     }
--- RSOUserMicroServiceAPI/GrpcService1/Program.cs
+++ RSOUserMicroServiceAPI/GrpcService1/Program.cs
@@ -1,6 +1,7 @@
 using GrpcService1.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using RSO.Core.BL;
 using RSO.Core.Configurations;
 using RSO.Core.Repository;
@@ -34,16 +35,32 @@ options.UseNpgsql(builder.Configuration.GetConnectionString("UserServicesRSOdB")
 
 
             builder.Services.AddLazyCache();
+
             // Add services to the container.
-            builder.Services.AddGrpc();
+            builder.Services.AddGrpc().AddJsonTranscoding();
+            builder.Services.AddGrpcSwagger();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo { Title = "gRPC transcoding", Version = "v1" });
+            });
+
+
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddScoped<IUserRepository, UserRepository>(); //In each microservice a different repository/serice is inlcuded. If more tables are needed add more repos related to the microservice.
             builder.Services.AddScoped<IUserLogic, UserLogic>(); //In each microservice a different repository/serice is inlcuded.
             var app = builder.Build();
 
             app.MapGrpcService<GreeterService>();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
             app.Run();
         }
     }
 }
+
+//https://www.youtube.com/watch?v=ds27V6rNick
